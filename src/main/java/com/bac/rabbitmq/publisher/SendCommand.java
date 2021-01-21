090de9c881c6310390e1eb12c2bc3cd78f5a6b48
Shahnawaz1985 <shahnawaz.akhter@outlook.com>
2021-01-21 22:05:28 +0530

    Basic sender receiver api for RabbitMQ.

---
+++ src/main/java/com/bac/rabbitmq/publisher/SendCommand.java
@@ -0,0 +1,52 @@
+package com.bac.rabbitmq.publisher;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * 
+ * @author Shahnawaz
+ *
+ */
+public class SendCommand {
+	
+	private static final String QUEUE_NAME = "hello";
+	private static final String CONNECTION_URL = "amqp://guest:guest@localhost:5673";
+	
+	public static void main(String[] args) {
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		try {
+			factory.setUri(CONNECTION_URL);
+		} catch (KeyManagementException | NoSuchAlgorithmException | URISyntaxException e1) {
+			System.out.println("Exception thrown while configuring uri ["+CONNECTION_URL+"]");
+			e1.printStackTrace();
+		}
+		/*
+		 * factory.setHost("localhost"); factory.setPassword("guest");
+		 * factory.setUsername("guest"); factory.setPort(5673); factory.set
+		 */
+		try(Connection conn = factory.newConnection()){
+			
+			Channel channel  = conn.createChannel();
+			channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+			String message = "Hello World RabbitMQ!";
+			channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+			
+			System.out.println("[x] Sent '" + message + "'");
+		} catch (IOException | TimeoutException e) {
+		  //catch (Throwable e) {
+			System.out.println("Exception captured while sending message on ["+QUEUE_NAME+"]");
+			e.printStackTrace();
+		}
+	}
+			
+
+}
---
+++ src/main/java/com/bac/rabbitmq/receiver/ReceiveCommand.java
@@ -0,0 +1,56 @@
+package com.bac.rabbitmq.receiver;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ * 
+ * @author Shahnawaz
+ *
+ */
+public class ReceiveCommand {
+	
+	private static final String QUEUE_NAME = "hello";
+	private static final String CONNECTION_URL = "amqp://guest:guest@localhost:5673";
+	
+	public static void main(String[] args) {
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		//factory.setHost(CONNECTION_URL);
+		
+		try {
+			factory.setUri(CONNECTION_URL);
+		} catch (KeyManagementException | NoSuchAlgorithmException | URISyntaxException e1) {
+			System.out.println("Exception thrown while configuring uri ["+CONNECTION_URL+"]");
+			e1.printStackTrace();
+		}
+		
+		try(Connection conn = factory.newConnection()){
+			
+			Channel channel  = conn.createChannel();
+			channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+			System.out.println("[*] Waiting for messages from SendCommnad. Press Ctrl-C to exit.");
+			
+			final DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+				final String message = new String(delivery.getBody(), "UTF-8");
+				System.out.println("[x] Received '" + message + "'");
+			};
+			
+			channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});			
+		} catch (IOException | TimeoutException e) {
+			//catch (Throwable e) {
+			System.out.println("Exception captured while receiving message on ["+QUEUE_NAME+"]");
+			e.printStackTrace();
+		}
+		 
+	}
+
+}
